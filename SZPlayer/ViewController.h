03f0351232383591abcc1d63379618e2c1a4d97f
SongZhen <591555331@qq.com>
2016-05-25 14:47:48 +0800

    Initial Commit

---
+++ SZPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SZPlayer
+//
+//  Created by 又土又木 on 16/5/25.
+//  Copyright © 2016年 ytuymu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SZPlayer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SZPlayer
+//
+//  Created by 又土又木 on 16/5/25.
+//  Copyright © 2016年 ytuymu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
